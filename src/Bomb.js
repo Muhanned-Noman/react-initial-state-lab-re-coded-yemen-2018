28c52d157b0425c51223a81453e527e8a7d8d3bb
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-12 15:21:26 +0000

    automatically backed up by learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -15,7 +15,7 @@ class Bomb extends React.Component {
 
   render() {
     return (
-      <h1>seconds left before I go boom!{this.state.currentSlideIndex}</h1>
+      <h1>{this.state.secondsLeft}seconds left before I go boom!</h1>
     )
   }
 }
