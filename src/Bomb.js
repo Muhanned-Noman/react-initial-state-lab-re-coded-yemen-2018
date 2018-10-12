c53b6ad3f9418cb7a7422c75dd3f75a994590c9c
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-12 15:19:55 +0000

    automatically backed up by learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -3,3 +3,19 @@
 import React from 'react';
 
 
+class ImageSlider extends React.Component {
+
+  constructor() {
+    super() 
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+    return (
+      <h1>I am on slide {this.state.currentSlideIndex}</h1>
+    )
+  }
+}
